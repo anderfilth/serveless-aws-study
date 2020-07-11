16fbb936e536ab69ba5e763ae76eca9282adbc6a
A. Ribeiro dos Santos <anderfilth@hotmail.com>
2020-07-11 12:45:47 -0300

    -

--- 06-layers/layers-nodejs-modules/handler.js
+++ 06-layers/layers-nodejs-modules/handler.js
@@ -6,6 +6,4 @@ module.exports.hello = async event => {
     body: moment().format(),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
